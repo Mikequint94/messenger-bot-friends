dfd34da6881f0ef1ae916943c75109806ed99036
Michael Quint <mjq8@cornell.edu>
2018-11-15 23:02:49 -0800

    added help text and setting up for group list

---
+++ README.txt
@@ -0,0 +1,6 @@
+TO-DO List With Friends!
+
+Goals:
+1.  Allow for scheduling reminders at certain times in the future
+2.  Add things and remove things from list
+3.  Create shared lists with friends. (create new table, get special share code to send to a friend, add things to list which notifies other person)
--- index.js
+++ index.js
@@ -9,33 +9,25 @@ const
   PAGE_ACCESS_TOKEN = "EAAIZANhHSqH4BAI7Y9Q6EAHH9DCNSWmRw9vVTXIjW6B4ZBBASOKeLc3CYLcmoZCCFLiSUnarTkWpDFiJKblPPShtYpbHhZCnFTa7fIzKK0mWZBFwBgrXv4BbpCPy8lZB4PINyQg06vpzTORPlOAbMazKAcRzZA3mfSNl6DkMm4degZDZD";
 const schedule = require('node-schedule');
 const chrono = require('chrono-node');
-
+const { Client } = require('pg');
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
-const { Client } = require('pg');
-
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
-
   let body = req.body;
-
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
-
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
-
       // Gets the message. entry.messaging is an array, but
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
-
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
       console.log('Sender PSID: ' + sender_psid);
-
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
       if (webhook_event.message) {
@@ -44,37 +36,29 @@ app.post('/webhook', (req, res) => {
         handlePostback(sender_psid, webhook_event.postback);
       }
     });
-
     // Returns a '200 OK' response to all requests
     res.status(200).send('EVENT_RECEIVED');
   } else {
     // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
-
 });
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
-
   // Your verify token. Should be a random string.
   const VERIFY_TOKEN = "a4b8c15d16e23f42";
-
   // Parse the query params
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
-
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
-
     // Checks the mode and token sent is correct
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
       // Responds with the challenge token from the request
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
-
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);
@@ -84,28 +68,33 @@ app.get('/webhook', (req, res) => {
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
-
   let response;
+  let senderId = sender_psid.toString();
+  let setListName = {};
   const greetings = ['hi', 'hello', 'hola', 'sup', 'whatsup', 'yo', 'hey', 'heyy', 'heyyy', 'whats up', 'what\'s up'];
   const lovingMessages = ['i love you', '143', 'i <3 you', 'i love u', 'i love you so much'];
-  const requestReminders = ['what are my reminders?', 'reminders list', 'tell me my reminders', 'what do I have scheduled?', 'reminders?'];
+  const requestReminders = ['what are my reminders?', 'what are my reminders', 'reminders list', 'tell me my reminders', 'what do I have scheduled?', 'reminders?'];
+  const helpRequest = ['help', 'help me', 'what can you do?', 'what can you do'];
+  const friendListRequest = ['start a list with friends', 'make a new friend list', 'make list with friends', 'make list with a friend', 'new friend list', 'start a new list with a friend', 'make new list', 'new list', 'make new friend list'];
   // Check if the message contains text
   if (received_message.text) {
+    if (setListName.senderId) {
+      let listName = received_message.text;
+      // let randomString = 
+      response = {
+        "text": `Very well.  Have your friend message me the following string to create the joint list.`
+      };
+    }
     let reminderText = isReminder(received_message.text.toLowerCase());
     if (lovingMessages.indexOf(received_message.text.toLowerCase()) !== -1) {
       response = {
         "text": `I LOVE YOU TOO!!!`
       };
-      var j = schedule.scheduleJob('32 * * * *', function(){
-        callSendAPI(sender_psid, {
-          "text": 'The answer to life, the universe, and everything!'
-        });
-      });
-      var k = schedule.scheduleJob('33 * * * *', function(){
-        callSendAPI(sender_psid, {
-          "text": 'Its the 29th min'
-        });
-      });
+      // var j = schedule.scheduleJob('32 * * * *', function(){
+      //   callSendAPI(sender_psid, {
+      //     "text": 'The answer to life, the universe, and everything!'
+      //   });
+      // });
     } else if (greetings.indexOf(received_message.text.toLowerCase()) !== -1) {
       response = {
         "text": greetings[Math.floor(Math.random()*(greetings.length - 1))]
@@ -114,15 +103,24 @@ function handleMessage(sender_psid, received_message) {
         response = {
           "text":  `OK.  I will remind you ${reminderText}`
         };
-        setReminder(reminderText, sender_psid.toString());
+        setReminder(reminderText, senderId);
     } else if (requestReminders.indexOf(received_message.text.toLowerCase()) !== -1) {
         response = {
-          "text":  readReminders(sender_psid.toString())
+          "text":  readReminders(senderId)
         };
+    } else if (helpRequest.indexOf(received_message.text.toLowerCase()) !== -1) {
+        response = {
+          "text":  'Welcome to To-Do With Friends!\nIf you\'d like to add something to your list just say remind me to ... \n To see your reminders ask "what are my reminders?" \n to start a list with a friend just say so!'
+        };
+    } else if (friendListRequest.indexOf(received_message.text.toLowerCase()) !== -1) {
+        response = {
+          "text":  'What would you like to call this list? (friend\'s name, nickname, list objective, etc.)'
+        };
+        setListName[senderId] = true;
     } else {
       // Create the payload for a basic text message
       response = {
-        "text": `:) You sent the message: "${received_message.text}".  <3 Now send me an image! :P`
+        "text": `:) You sent the message: "${received_message.text}".  <3 Now send me an image! :P  or ask for help!`
       };
     }
 
@@ -225,10 +223,8 @@ function readReminders(senderId) {
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
   let response;
-
   // Get the payload for the postback
   let payload = received_postback.payload;
-
   // Set the response based on the postback payload
   if (payload === 'yes') {
     response = { "text": "Thanks!" };
@@ -248,7 +244,6 @@ function callSendAPI(sender_psid, response) {
     },
     "message": response
   };
-
   // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
